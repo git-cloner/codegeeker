0db7d8fc461e65392850a9b346c01e75dbb10300
simon gao <little51@126.com>
2022-12-01 10:55:35 +0800

    V0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# codegeeker
-VSCode extensions: Code generator base on salesforce codegen
+# Codegeeker
+Code generation Extension based on SalesForce CodeGen.
+
+Source code of Extension: https://github.com/git-cloner/codegeeker
+
+Source code of Codegen : https://github.com/git-cloner/codegen
+
+Online demo: https://gitclone.com/aiit/codegen
+
+## Extension usage
+
+If you use Python or PlainText, enter a colon (:) in the VScode editor to display the generated candidate code snippet.
+
+If it is C, C++, Go, JavaScript, Java, etc., enter { in the VSCODE editor to display the generated candidate code fragment.
+
+## Implementation principle
+
+Enter triggerCharacters (:or {), post current line to the backend (https://github.com/git-cloner/codegen) to generate the code, and the backend uses salesforce's CodeGen model (https://github.com/salesforce/CodeGen) to generate the code snippet.
+
+## 中文说明
+
+基于 salesforce codegen的代码生成插件。
+
+插件源码：https://github.com/git-cloner/codegeeker
+
+生成源码：https://github.com/git-cloner/codegen
+
+在线演示：https://gitclone.com/aiit/codegen
+
+### 1、插件用法
+
+如果是python或plaintext，在vscode编辑器中输入冒号（:），显示生成的候选代码片断。
+
+如果是c、c++、go、javascript、java等，在vscode编辑器中输入{，显示生成的候选代码片断。
+
+#### 2、实现原理
+
+输入triggerCharacters（:或{）,将本行代码post到后台（https://github.com/git-cloner/codegen）生成代码，后台使用salesforce的CodeGen模型（https://github.com/salesforce/CodeGen）生成代码片断返回。
+
--- src/extension.ts
+++ src/extension.ts
@@ -6,40 +6,51 @@ import axios from 'axios';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const provider = vscode.languages.registerCompletionItemProvider(["python", "plaintext", 'cpp', 'c', 'java', 'javascript'], {
+	const provider = vscode.languages.registerCompletionItemProvider(["python", "plaintext"], {
 		async provideCompletionItems(doc, pos) {
-			let currentLine = doc.lineAt(pos).text;
-			if (!currentLine.endsWith(":") && !currentLine.endsWith("{") && !currentLine.endsWith("}")) {
-				return undefined;
-			};
-			//vscode.window.showInformationMessage('Hello World from Codegeeker!');
-			const json = JSON.stringify({ "context": currentLine.replace("..", ""), "maxlength": 64 });
-			const res = await axios.post('https://gitclone.com/aiit/codegen', json, {
-				headers: {
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					"Content-Type": "application/json"
-				}
-			});
-			var allCode = res.data.result ;
-			let completionItems:vscode.CompletionItem[] = [] ;
-			if (allCode){
-				var sl = allCode.split("\n");
-				for(var i=0;i<sl.length;i++){
-					const commandCompletion = new vscode.CompletionItem(sl[i]);
-					commandCompletion.kind = vscode.CompletionItemKind.Text;
-					commandCompletion.detail = allCode ;
-					commandCompletion.documentation = new vscode.MarkdownString(allCode);
-					commandCompletion.insertText = new vscode.SnippetString(allCode);
-					completionItems.push(commandCompletion) ;
-				}
-				return completionItems ;				
-			}else{
-				return undefined ;
-			}
+			return getCompletionItemsFromRemote(doc, pos);
 		}
 	}, ':');
-	context.subscriptions.push(provider);
+	const provider1 = vscode.languages.registerCompletionItemProvider(["python", "plaintext", "cpp", "c", "java", "javascript","go"], {
+		async provideCompletionItems(doc, pos) {
+			return getCompletionItemsFromRemote(doc, pos);
+		}
+	}, '{');
+	context.subscriptions.push(provider, provider1);
 }
+
+async function getCompletionItemsFromRemote(doc: vscode.TextDocument, pos: vscode.Position) {
+	let currentLine = doc.lineAt(pos).text;
+	if (!currentLine.endsWith(":") && !currentLine.endsWith("{") && !currentLine.endsWith("}")) {
+		return undefined;
+	};
+	//vscode.window.showInformationMessage('Hello World from Codegeeker!');
+	const json = JSON.stringify({ "context": currentLine.replace("..", ""), "maxlength": 64 });
+	const res = await axios.post('https://gitclone.com/aiit/codegen', json, {
+		headers: {
+			// eslint-disable-next-line @typescript-eslint/naming-convention
+			"Content-Type": "application/json"
+		}
+	});
+	vscode.workspace.getConfiguration
+	var allCode = res.data.result;
+	let completionItems: vscode.CompletionItem[] = [];
+	if (allCode) {
+		var sl = allCode.split("\n");
+		for (var i = 0; i < sl.length; i++) {
+			const commandCompletion = new vscode.CompletionItem(sl[i]);
+			commandCompletion.kind = vscode.CompletionItemKind.Text;
+			commandCompletion.detail = allCode;
+			commandCompletion.documentation = new vscode.MarkdownString(allCode);
+			commandCompletion.insertText = new vscode.SnippetString(allCode);
+			completionItems.push(commandCompletion);
+		}
+		return completionItems;
+	} else {
+		return undefined;
+	}
+}
+
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
